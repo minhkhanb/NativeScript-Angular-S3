696c3d82de807c0ed6deef6a3db254e18a31233e
Kha Le <minhkhanb@gmail.com>
2019-02-27 16:11:55 +0700

    update dimension gridlayout

--- src/app/toiec/toiec.component.ts
+++ src/app/toiec/toiec.component.ts
@@ -14,23 +14,132 @@ const ITEM_WIDTH_PERCENT = 0.33333;
 
 export class ToiecComponent implements OnInit {
 
-  itemWidth: number = screen.mainScreen.widthDIPs * ITEM_WIDTH_PERCENT;
-  itemHeight: number = this.itemWidth;
+  itemWidth: number = (screen.mainScreen.widthDIPs / 3);
+  itemHeight: number = this.itemWidth * 9 / 16;
 
-  totalItem: string[] = [
-    
+  itemColor: string = '#555';
+  itemRows: string = '';
+
+  totalItem: Array<any> = [
+    {
+      id: 0,
+      bgColor: '',
+    },
+    {
+      id: 1,
+      bgColor: '',
+    },
+    {
+      id: 2,
+      bgColor: '',
+    },
+    {
+      id: 3,
+      bgColor: '',
+    },
+    {
+      id: 4,
+      bgColor: '',
+    },
+    {
+      id: 5,
+      bgColor: '',
+    },
+    {
+      id: 6,
+      bgColor: '',
+    },
+    {
+      id: 7,
+      bgColor: '',
+    },
+    {
+      id: 8,
+      bgColor: '',
+    },
+    {
+      id: 9,
+      bgColor: '',
+    },
+    {
+      id: 10,
+      bgColor: '',
+    },
+    {
+      id: 11,
+      bgColor: '',
+    },
+    {
+      id: 12,
+      bgColor: '',
+    },
+    {
+      id: 13,
+      bgColor: '',
+    },
+    {
+      id: 14,
+      bgColor: '',
+    },
+    {
+      id: 15,
+      bgColor: '',
+    },
+    {
+      id: 16,
+      bgColor: '',
+    },
+    {
+      id: 17,
+      bgColor: '',
+    },
+    {
+      id: 18,
+      bgColor: '',
+    },
+    {
+      id: 19,
+      bgColor: '',
+    },
   ];
   
   constructor() {
-
+    app.on('orientationChanged', this.onOrientationChanged);
   }
 
   ngOnInit(): void {
+    this.totalItem.map(item => {
+      item.bgColor = this.getRandomInt();
+      item.title = 'Plan ' + item.id;
+      item.col = item.id % 3;
+      item.row = Math.floor(item.id / 3);
+    });
+    this.itemRows = this.getItemRows();
+    console.log('totalItem: , this.itemRows: ', this.totalItem, this.itemRows);
+  }
 
+  public onOrientationChanged = (evt) => {
+    console.log('Orientation changed  !: ', evt);
+  }
+
+  getItemRows(): any {
+    const rows = Math.floor(this.totalItem.length / 3);
+    let rowsString = 'auto';
+    for( let i = 0; i < rows; i++) {
+      rowsString += ' auto';
+    }
+    return rowsString;
   }
   
   onDrawerButtonTap(): void {
     const sideDrawer = <RadSideDrawer>app.getRootView();
     sideDrawer.showDrawer();
+    sideDrawer.resetNativeView();
   }
+
+  getRandomInt() {
+    return '#' + Math.random().toString(16).slice(2, 8);
+  }
+
+
 }
