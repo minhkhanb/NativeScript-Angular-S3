081fd9e1cb91fee06c263b47386e3470b87111d2
Kha Le <minhkhanb@gmail.com>
2019-02-28 14:52:20 +0700

    add videoplayer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 
+import { Page } from 'ui/page';
+
 import { YoutubePlayer } from 'nativescript-youtubeplayer';
 
 @Component({
@@ -11,31 +13,16 @@ import { YoutubePlayer } from 'nativescript-youtubeplayer';
 })
 export class HomeComponent implements OnInit {
 
+    _page;
+
     src = 'Hk1JY7vsuWg';
     
     options = { rel: 1 };
     apiKey = 'AIzaSyCiprq85s9ISFqie9jBEBrps6_g-4ca3lo';
-    ytplayer: YoutubePlayer;
 
-    toggleFullScreen() {
-        this.ytplayer.toggleFullscreen();
-    }
-
-    changeSource() {
-        this.src = 'dQw4w9WgXcQ';
-    }
-    getDanceHall() {
-        this.src = 'wH_0_pijbZY';
-    }
-    getHipHop() {
-        this.src = 'lwk5OUII9Vc';
-    }
-    getSoca() {
-        this.src = 'vEG4qNW33mA';
-    }
-
-    constructor() {
+    constructor(private page: Page) {
         // Use the component constructor to inject providers.
+        this._page = page;
         app.on('orientationChanged', this.onOrientationChanged);
     }
 
@@ -55,5 +42,31 @@ export class HomeComponent implements OnInit {
     onOrientationChanged = (evt) => {
         console.log('set full screen youtube player');
         
+    }
+
+    toggleFullScreen() {
+        const player = this._page.getViewById('ytplayer');
+        if (player && !player.isPlaying()) {
+            player.play();
+        }
+        if (player) {
+            player.toggleFullScreen();
+        }
+        console.log('set video fullscreen: ', player);
+        // this.ytVideo.toggleFullscreen();
+        
+    }
+
+    changeSource() {
+        this.src = 'dQw4w9WgXcQ';
+    }
+    getDanceHall() {
+        this.src = 'wH_0_pijbZY';
+    }
+    getHipHop() {
+        this.src = 'lwk5OUII9Vc';
+    }
+    getSoca() {
+        this.src = 'vEG4qNW33mA';
     }
 }
